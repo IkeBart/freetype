dca9f6771bbb3c93c06f7080f48e6cb104b773cb
Isaac Barrett <ikebart9999@gmail.com>
2015-06-19 14:03:54 -0700

    Changed things to fix one error in XCode.

--- src/gzip/infutil.h
+++ src/gzip/infutil.h
@@ -86,7 +86,11 @@ struct inflate_blocks_state {
 
 /* masks for lower bits (size given to avoid silly warnings with Visual C++) */
 #ifndef NO_INFLATE_MASK
-local uInt inflate_mask[17];
+local const uInt inflate_mask[17] = {
+    0x0000,
+    0x0001, 0x0003, 0x0007, 0x000f, 0x001f, 0x003f, 0x007f, 0x00ff,
+    0x01ff, 0x03ff, 0x07ff, 0x0fff, 0x1fff, 0x3fff, 0x7fff, 0xffff
+};
 #endif
 
 /* copy as much as possible from the sliding window to the output area */
